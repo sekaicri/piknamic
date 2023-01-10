5af2bf5f4891eba1cb01c2b8820ba53809498bc8
Json55Hdz <jeisondh55@gmail.com>
2023-01-10 13:01:14 -0500

    cdcd

--- app/Models/project.php
+++ app/Models/project.php
@@ -3,9 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class project extends Model
+class project extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'preview',
+        'information',
+        'user_id',
+    ];
 }
