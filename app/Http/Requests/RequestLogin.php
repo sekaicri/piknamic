2963cc6927591e9928a09bdae200cee5a20a0bbd
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-01-28 08:19:33 -0500

    TGTGT

--- app/Http/Requests/RequestLogin.php
+++ app/Http/Requests/RequestLogin.php
@@ -19,7 +19,7 @@ class RequestLogin extends FormRequest
     {
         return [
             'password'=>'required|string',
-            'email'=>'required|string|email',
+            'email'=>'required|string|email|unique',
         ];
     }
     public function failedValidation(Validator $validator)
