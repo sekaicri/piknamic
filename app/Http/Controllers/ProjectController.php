61a474d1478e556e252fbea4e5b15fb07186cfc8
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-02-14 07:45:06 -0500

    Update https imagens

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -24,7 +24,7 @@ class ProjectController extends Controller
             } else {
                 $filename = Null;
             }
-            $filename = 'http://www.piknamic.com/storage/' . $request->project_id . '/' . $filename;
+            $filename = 'https://piknamic.com/storage/' . $request->project_id . '/' . $filename;
             // inicio de transaccion
             DB::beginTransaction();
             $user = imagens::create([
