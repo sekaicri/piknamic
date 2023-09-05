d430d529077efd70ac6b995b09ad0d5eee409d89
CFRicaurte <cristianricaurte-externo@tgacompany.com>
2023-09-04 19:32:31 -0500

    Update home

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['prefix' => 'editor'], function () {
-    Route::get('/', function () {
-        return view('Editor.index');
-    });
-});
